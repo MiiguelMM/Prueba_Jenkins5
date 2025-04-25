303f68a73bdd2c11452d0ccd5d6a9524341edc90
MiiguelMM <miguelmelgarejosanchez@live.com>
2025-04-25 13:53:08 +0200

    HOLA

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,34 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
+        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
+        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
+        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
+        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
+        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
+        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
+        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
+        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
+        System.out.println("Hello world!");
+        System.out.println("Hola");
+        System.out.println("Perro Sanchez");
     }
 }
